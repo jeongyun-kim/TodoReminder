3e1e41a38a83df1d56afe6399b52db888baf9669
jeongyun-kim <kkanyo04@naver.com>
2024-07-02 19:54:26 +0900

    Feat: Todo 모델 생성

--- TodoReminder/Model/Todo.swift
+++ TodoReminder/Model/Todo.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import RealmSwift
+
+class Todo: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title: String
+    @Persisted var content: String?
+    @Persisted var deadline: Date?
+    @Persisted var tag: String?
+    @Persisted var priority: String?
+    @Persisted var imageName: String?
+    @Persisted var isFlag: Bool
+    @Persisted var isComplete: Bool
+    @Persisted var savedate: Date
+    
+    convenience init(id: ObjectId, title: String, content: String? = nil, deadline: Date? = nil, tag: String? = nil, priority: String? = nil, imageName: String? = nil, savedate: Date) {
+        self.init()
+        self.title = title
+        self.content = content
+        self.deadline = deadline
+        self.tag = tag
+        self.priority = priority
+        self.imageName = imageName
+        self.isFlag = false
+        self.isComplete = false
+        self.savedate = savedate
+    }
+}
