4040b31b6ff8b0fbb8a6c00e546a2fec31356ce5
jeongyun-kim <kkanyo04@naver.com>
2024-07-03 00:30:01 +0900

    Feat: ListVC 내 TableView 구현(1)
    제목, 메모까지만 표출

--- TodoReminder/Cell/ListTableViewCell.swift
+++ TodoReminder/Cell/ListTableViewCell.swift
@@ -5,4 +5,75 @@
 //  Created by 김정윤 on 7/2/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class ListTableViewCell: BaseTableViewCell {
+    private let checkBox: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: Resource.ImageCase.inProgress.rawValue)
+        imageView.tintColor = .lightGray
+        return imageView
+    }()
+    private let titleLabel = UILabel()
+    private let memoLabel = UILabel()
+    private let descStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 4
+        return stackView
+    }()
+    private let deadlineLabel = UILabel()
+    private let tagLabel = UILabel()
+    
+    override func setupHierarchy() {
+        contentView.addSubview(checkBox)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(memoLabel)
+        contentView.addSubview(descStackView)
+        [deadlineLabel, tagLabel].forEach {
+            descStackView.addArrangedSubview($0)
+        }
+    }
+    
+    override func setupConstraints() {
+        checkBox.snp.makeConstraints { make in
+            make.size.equalTo(25)
+            make.top.equalTo(contentView.safeAreaLayoutGuide).offset(8)
+            make.leading.equalTo(contentView.safeAreaLayoutGuide).offset(16)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(checkBox.snp.centerY)
+            make.leading.equalTo(checkBox.snp.trailing).offset(8)
+            make.height.equalTo(20)
+            make.trailing.equalTo(contentView.safeAreaLayoutGuide).inset(16)
+        }
+        
+        memoLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(titleLabel)
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+        }
+        
+        descStackView.snp.makeConstraints { make in
+            make.leading.equalTo(memoLabel.snp.leading)
+            make.top.equalTo(memoLabel.snp.bottom).offset(4)
+            make.bottom.equalTo(contentView.safeAreaLayoutGuide).inset(8)
+        }
+    }
+    
+    override func configureLayout() {
+        super.configureLayout()
+        titleLabel.font = Resource.FontCase.regular16
+        memoLabel.font = Resource.FontCase.regular15
+        memoLabel.textColor = .lightGray
+        deadlineLabel.textColor = .lightGray
+        tagLabel.textColor = .systemBlue
+        deadlineLabel.backgroundColor = .white
+    }
+    
+    func configureCell(_ data: Todo) {
+        titleLabel.text = data.title
+        memoLabel.text = data.memo
+    }
+}
--- TodoReminder/VC/ListViewController.swift
+++ TodoReminder/VC/ListViewController.swift
@@ -5,4 +5,71 @@
 //  Created by 김정윤 on 7/2/24.
 //
 
-import Foundation
+import UIKit
+import RealmSwift
+import SnapKit
+
+class ListViewController: BaseViewControllerLargeTitle {
+    private let realm = try! Realm()
+    private let tableView = UITableView()
+    private var list: Results<Todo>! { // IUO
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    var navigationTitle: String = "전체"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        list = realm.objects(Todo.self)
+    }
+    
+    override func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    override func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func setupUI() {
+        super.setupUI()
+    }
+    
+    
+    override func setupNavigation(_ title: String, rightItemTitle: String?, rightItemImage: String?, action: Selector?) {
+        super.setupNavigation(navigationTitle, rightItemTitle: nil, rightItemImage: "plus", action: #selector(addBtnTapped))
+    }
+    
+    override func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ListTableViewCell.self, forCellReuseIdentifier: ListTableViewCell.identifier)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+    }
+    
+    @objc func addBtnTapped(_ sender: UIButton) {
+        let vc = AddViewController()
+        //let navi = UINavigationController(rootViewController: vc)
+        transition(vc, type: .push)
+    }
+}
+
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.identifier, for: indexPath) as! ListTableViewCell
+        cell.configureCell(list[indexPath.row])
+        return cell
+    }
+}
