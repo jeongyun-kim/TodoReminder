6a44862c3ba97fb7848f963d1ddb639259f4c6a5
jeongyun-kim <kkanyo04@naver.com>
2024-07-04 22:37:27 +0900

    Feat: Date 비교 관련 메서드 구성 / 어떤 VC에 사용되는지에 따라 Date를 다른 형태로 변환하여 String을 반환하는 메서드 구성

--- TodoReminder/Extension/Date + Extension.swift
+++ TodoReminder/Extension/Date + Extension.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 extension Date {
+    // 현재 날짜랑 마감일에서 시간은 동일하게 -> 년월일만 비교할 수 있게
+    static func getRemovedTimeDate(preDate: Date, preDeadline: Date) -> [Date] {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY-MM-dd"
+        
+        let todayDateString = formatter.string(from: preDate)
+        let deadlineString = formatter.string(from: preDeadline)
+        
+        if let postDate = formatter.date(from: todayDateString), let postDeadline = formatter.date(from: deadlineString) {
+            return [postDate, postDeadline]
+        } else {
+            return []
+        }
+    }
+    
     static func dateCompare(deadline: Date?) -> Resource.DateCompareCase {
         guard let deadline else { return .none }
-        let result:ComparisonResult = Date(timeIntervalSinceNow: 32400).compare(deadline)
+        // 마감일과 오늘 날짜 비교할 때, 시간은 제외하고 년월일만 비교해줘야함
+        // -> 현재 날짜, 마감일 보내서 두 데이터 모두 시간 제외한 Date 타입으로 새로 가져와서 비교하기
+        let postDateArray = getRemovedTimeDate(preDate: Date(), preDeadline: deadline)
+        
+        let postDate = postDateArray[0]
+        let postDeadline = postDateArray[1]
+        
+        let result:ComparisonResult = postDate.compare(postDeadline)
         switch result {
         case .orderedAscending:
             return .future
@@ -21,5 +43,14 @@ extension Date {
         default:
             return .none
         }
+    }
+    
+    // AddVC 내 테이블뷰셀에 들어가는지 / ListVC 내 테이블뷰 셀에 들어가는지에 따라 dateFormat 다르게해서 String으로 변환한 결과 보내기 
+    static func dateFormattedString(_ date: Date, type: Resource.DateFormatUsage) -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = type == .list ? "YYYY.M.d." : "YYYY.MM.dd (EEEEE)"
+        let result = formatter.string(from: date)
+        return result
     }
 }
