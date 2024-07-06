3e7dd6148cfe37ca26f3931573f889d4c8a148e7
jeongyun-kim <kkanyo04@naver.com>
2024-07-06 16:02:23 +0900

    Feat: CalendarVC 내 FSCalendar 이용해 선택한 날짜가 마감일인 할 일 가져와 보여주기

--- TodoReminder/Database/TodoRepository.swift
+++ TodoReminder/Database/TodoRepository.swift
@@ -21,14 +21,14 @@ final class TodoRepository {
         }
     }
     
-    func readAllItems(_ sortType: Resource.ListSortType = .dateAsc) -> [Todo] {
+    func readAllItems(_ sortType: Resource.ListSortType = .dateAsc) -> Results<Todo> {
         let sortType = sortType == .dateAsc
         // savedate를 기준으로 오름차순 = true / 내림차순 = false
-        return Array(realm.objects(Todo.self).sorted(byKeyPath: "savedate", ascending: sortType))
+        return realm.objects(Todo.self).sorted(byKeyPath: "savedate", ascending: sortType)
     }
     
     func readFilteredItem(_ filter: ReminderCase, sort: Resource.ListSortType = .dateAsc) -> [Todo] {
-        let allData = readAllItems(sort)
+        let allData = Array(readAllItems(sort))
         switch filter {
         case .today:
             return allData.filter { Date.dateCompare(deadline: $0.deadline) == .today}
@@ -49,13 +49,23 @@ final class TodoRepository {
         // 받아온 리스트
         let originalListSet = Set(list)
         // 전체 리스트에서 제목, 메모에 키워드가 포함된 검색결과 받아오기
-        let searchedListSet = Set(realm.objects(Todo.self)
-            .where { $0.todoTitle.contains(keyword, options: .caseInsensitive) || $0.memo.contains(keyword, options: .caseInsensitive) })
+        let searchedListSet = Set(readAllItems()
+            .where { $0.todoTitle.contains(keyword, options: .caseInsensitive)
+                || $0.memo.contains(keyword, options: .caseInsensitive) })
         // 받아온 리스트랑 전체 내 검색결과의 교집합으로 구성해주기 (= 리스트 내 검색 결과를 구할 수 있음)
         let result = originalListSet.intersection(searchedListSet)
         return Array(result)
     }
     
+    func readFilteredDateItem(_ date: Date) -> [Todo] {
+        let allData = Array(readAllItems())
+        let nonNilDeadline = allData.filter { $0.deadline != nil }
+        let result = nonNilDeadline.filter {
+            Date.dateFormattedString($0.deadline!)
+            == Date.dateFormattedString(date)
+        }
+        return result
+    }
     
     func updateItem(_ completionHandler: () -> Void)  {
         do {
@@ -78,3 +88,5 @@ final class TodoRepository {
         }
     }
 }
+
+
--- TodoReminder/Main/CalendarViewController.swift
+++ TodoReminder/Main/CalendarViewController.swift
@@ -6,22 +6,100 @@
 //
 
 import UIKit
+import FSCalendar
+import SnapKit
 
 final class CalendarViewController: BaseViewController {
+    private let repository = TodoRepository()
+    private var list: [Todo] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    private let calendar = FSCalendar()
+    private let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    override func setupNavigation(_ title: String) {
+        super.setupNavigation("캘린더")
+    }
+    
     override func setupHierarchy() {
-        
+        view.addSubview(calendar)
+        view.addSubview(tableView)
     }
     
     override func setupConstraints() {
+        calendar.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(calendar.snp.width)
+        }
         
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(calendar.snp.bottom)
+            make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ListTableViewCell.self, forCellReuseIdentifier: ListTableViewCell.identifier)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = UITableView.automaticDimension
     }
     
     override func setupUI() {
         super.setupUI()
+        calendar.delegate = self
+        calendar.dataSource = self
+        calendar.appearance.titleDefaultColor = .white
+        calendar.appearance.titleFont = Resource.FontCase.regular15
+        calendar.appearance.weekdayFont = Resource.FontCase.bold15
+        calendar.appearance.weekdayTextColor = .white
+        calendar.appearance.headerTitleColor = .white
+        calendar.appearance.headerTitleFont = Resource.FontCase.bold15
+        calendar.scrollDirection = .vertical
+        calendar.locale = Locale(identifier: "ko_KR")
+        calendar.appearance.todayColor = .systemGray5
+        calendar.appearance.selectionColor = ReminderCase.schedule.imageColor
+    }
+    
+    @objc public func completeBtnTapped(_ sender: UIButton) {
+        repository.updateItem {
+            list[sender.tag].isComplete.toggle()
+            tableView.reloadRows(at: [IndexPath(row: sender.tag, section: 0)], with: .none)
+        }
+    }
+}
+
+
+extension CalendarViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendar.snp.updateConstraints { make in
+            make.size.equalTo(bounds.size)
+        }
+        view.layoutIfNeeded()
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        list = repository.readFilteredDateItem(date)
+    }
+}
+
+extension CalendarViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.identifier, for: indexPath) as! ListTableViewCell
+        cell.configureCell(list[indexPath.row])
+        cell.completeButton.tag = indexPath.row
+        cell.completeButton.addTarget(self, action: #selector(completeBtnTapped), for: .touchUpInside)
+        return cell
     }
 }
